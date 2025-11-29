2d38db584b078be256ddfa66e48edfdc3e553a42
Mike Dilger <mike@mikedilger.com>
2025-11-30 08:08:14 +1300

    Add stricter compile requirements

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+//! Beermaker is a library for helping you make beer.
+
+#![warn(clippy::pedantic)]
+#![deny(
+    missing_debug_implementations,
+    trivial_numeric_casts,
+    clippy::string_slice,
+    unused_import_braces,
+    unused_results,
+    unused_lifetimes,
+    unused_labels,
+    unused_extern_crates,
+    non_ascii_idents,
+    keyword_idents,
+    deprecated_in_future,
+    unstable_features,
+    single_use_lifetimes,
+    unreachable_pub,
+    missing_copy_implementations,
+    missing_docs
+)]
+#![allow(
+    clippy::match_same_arms,
+    clippy::doc_markdown,
+)]
+#![cfg_attr(docsrs, feature(doc_auto_cfg))]
