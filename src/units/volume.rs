59ae490a4b698a0b9ba7068714cdc30199eef980
Mike Dilger <mike@mikedilger.com>
2025-12-27 12:42:59 +1300

    Add FluidOunces

--- src/units/volume.rs
+++ src/units/volume.rs
@@ -98,9 +98,38 @@ impl PartialOrd for Quarts {
     }
 }
 
+/// Volume in U.S fluid oz
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize, Add, Sum, Sub, Mul, Div)]
+pub struct FluidOunces(pub f32);
+
+impl fmt::Display for FluidOunces {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{:.1} fl oz", self.0)
+    }
+}
+
+impl Eq for FluidOunces {}
+
+impl Ord for FluidOunces {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.0.total_cmp(&other.0)
+    }
+}
+
+impl PartialOrd for FluidOunces {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+// Cup = 8 fl oz
+// Pint = 16 fl oz
+
 const MILLILITERS_PER_LITER: f32 = 1000.0;
 const LITERS_PER_GALLON: f32 = 3.7854;
 const QUARTS_PER_GALLON: f32 = 4.0;
+const FLUID_OUNCES_PER_GALLON: f32 = 128.0;
+const MILLILITERS_PER_FLUID_OUNCE: f32 = 29.5734375;
 
 impl From<Liters> for Milliliters {
     fn from(v: Liters) -> Self {
@@ -117,6 +146,12 @@ impl From<Gallons> for Milliliters {
 impl From<Quarts> for Milliliters {
     fn from(v: Quarts) -> Self {
         Milliliters(v.0 / QUARTS_PER_GALLON * LITERS_PER_GALLON * MILLILITERS_PER_LITER)
+    }
+}
+
+impl From<FluidOunces> for Milliliters {
+    fn from(v: FluidOunces) -> Self {
+        Milliliters(v.0 * MILLILITERS_PER_FLUID_OUNCE)
     }
 }
 
@@ -138,6 +173,12 @@ impl From<Quarts> for Liters {
     }
 }
 
+impl From<FluidOunces> for Liters {
+    fn from(v: FluidOunces) -> Self {
+        Liters(v.0 * MILLILITERS_PER_FLUID_OUNCE / MILLILITERS_PER_LITER)
+    }
+}
+
 impl From<Milliliters> for Gallons {
     fn from(v: Milliliters) -> Self {
         Gallons(v.0 / MILLILITERS_PER_LITER / LITERS_PER_GALLON)
@@ -153,6 +194,12 @@ impl From<Liters> for Gallons {
 impl From<Quarts> for Gallons {
     fn from(v: Quarts) -> Self {
         Gallons(v.0 / QUARTS_PER_GALLON)
+    }
+}
+
+impl From<FluidOunces> for Gallons {
+    fn from(v: FluidOunces) -> Self {
+        Gallons(v.0 / FLUID_OUNCES_PER_GALLON)
     }
 }
 
@@ -174,6 +221,36 @@ impl From<Gallons> for Quarts {
     }
 }
 
+impl From<FluidOunces> for Quarts {
+    fn from(v: FluidOunces) -> Self {
+        Quarts(v.0 / FLUID_OUNCES_PER_GALLON * QUARTS_PER_GALLON)
+    }
+}
+
+impl From<Milliliters> for FluidOunces {
+    fn from(v: Milliliters) -> Self {
+        FluidOunces(v.0 / MILLILITERS_PER_FLUID_OUNCE)
+    }
+}
+
+impl From<Liters> for FluidOunces {
+    fn from(v: Liters) -> Self {
+        FluidOunces(v.0 / MILLILITERS_PER_FLUID_OUNCE * MILLILITERS_PER_LITER)
+    }
+}
+
+impl From<Quarts> for FluidOunces {
+    fn from(v: Quarts) -> Self {
+        FluidOunces(v.0 / QUARTS_PER_GALLON * FLUID_OUNCES_PER_GALLON)
+    }
+}
+
+impl From<Gallons> for FluidOunces {
+    fn from(v: Gallons) -> Self {
+        FluidOunces(v.0 * FLUID_OUNCES_PER_GALLON)
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -188,6 +265,8 @@ mod test {
         assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
         let b = Into::<Milliliters>::into(Into::<Quarts>::into(a));
         assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
+        let b = Into::<Milliliters>::into(Into::<FluidOunces>::into(a));
+        assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
 
         let a = Liters(18.211);
         let b = Into::<Liters>::into(Into::<Milliliters>::into(a));
@@ -195,6 +274,8 @@ mod test {
         let b = Into::<Liters>::into(Into::<Gallons>::into(a));
         assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
         let b = Into::<Liters>::into(Into::<Quarts>::into(a));
+        assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
+        let b = Into::<Liters>::into(Into::<FluidOunces>::into(a));
         assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
 
         let a = Gallons(18.211);
@@ -204,6 +285,8 @@ mod test {
         assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
         let b = Into::<Gallons>::into(Into::<Quarts>::into(a));
         assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
+        let b = Into::<Gallons>::into(Into::<FluidOunces>::into(a));
+        assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
 
         let a = Quarts(18.211);
         let b = Into::<Quarts>::into(Into::<Milliliters>::into(a));
@@ -212,5 +295,17 @@ mod test {
         assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
         let b = Into::<Quarts>::into(Into::<Gallons>::into(a));
         assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
-    }
+        let b = Into::<Quarts>::into(Into::<FluidOunces>::into(a));
+        assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
+
+        let a = FluidOunces(18.211);
+        let b = Into::<FluidOunces>::into(Into::<Milliliters>::into(a));
+        assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
+        let b = Into::<FluidOunces>::into(Into::<Liters>::into(a));
+        assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
+        let b = Into::<FluidOunces>::into(Into::<Gallons>::into(a));
+        assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
+        let b = Into::<FluidOunces>::into(Into::<Quarts>::into(a));
+        assert!(approx_eq!(f32, a.0, b.0, ulps = 10));
+}
 }
